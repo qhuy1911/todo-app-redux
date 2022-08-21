79772969bd1c4cda0f5d60b922fc13349423382b
qhuy1911 <qhuy1911@gmail.com>
2022-08-21 21:28:07 +0700

    redux

---
+++ src/redux/selectors.js
@@ -0,0 +1,39 @@
+import {createSelector} from "reselect";
+
+export const searchTextSelector = (state) => state.filters.search;
+export const statusFilterSelector = (state) => state.filters.status;
+export const prioritiesFilterSelector = (state) => state.filters.priorities;
+export const todoListSelector = (state) => state.todoList;
+
+// export const todoListSelector = (state) => {
+//   const searchText = searchTextSelector();
+
+//   const todosRemaining = state.todoList.filter((todo) => {
+//     return todo.name.includes(searchText);
+//   });
+//   return todosRemaining;
+// };
+
+export const todosRemainingSelector = createSelector(
+  todoListSelector,
+  searchTextSelector,
+  statusFilterSelector,
+  prioritiesFilterSelector,
+  (todoList, searchText, statusFilter, prioritiesFilter) => {
+    return todoList.filter((todo) => {
+      if (statusFilter === "All") {
+        return prioritiesFilter.length
+          ? todo.name.includes(searchText) &&
+              prioritiesFilter.includes(todo.priority)
+          : todo.name.includes(searchText);
+      }
+      return (
+        todo.name.includes(searchText) &&
+        (statusFilter === "Completed" ? todo.completed : !todo.completed) &&
+        (prioritiesFilter.length
+          ? prioritiesFilter.includes(todo.priority)
+          : true)
+      );
+    });
+  }
+);
